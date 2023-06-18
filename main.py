6b451a825cb711de85f4897fb1fed39f046a6109
matrass3 <freeservermatras@gmail.com>
2023-06-18 17:59:38 +0300

    Код

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ from OpenGL.GL.shaders import compileShader, compileProgram
 import numpy as np
 import math
 
-
 pygame.init()
-
 
 width, height = 1200, 900
 screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
-
-
 texture = pygame.image.load("texture.jpg")  # Замените "texture.png" на путь к вашему изображению
 texture_data = pygame.image.tostring(texture, "RGBA", 1)
-
 
 vertex_shader = """
 #version 330
@@ -44,7 +39,6 @@ shader_program = compileProgram(
     compileShader(fragment_shader, GL_FRAGMENT_SHADER)
 )
 
-
 vertex_data = np.array([
     -1, -1, 0, 0,
     -1, 1, 0, 1,
@@ -55,12 +49,10 @@ vertex_buffer = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
 glBufferData(GL_ARRAY_BUFFER, vertex_data.nbytes, vertex_data, GL_STATIC_DRAW)
 
-
 index_data = np.array([0, 1, 2, 0, 2, 3], dtype=np.uint32)
 index_buffer = glGenBuffers(1)
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer)
 glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_data.nbytes, index_data, GL_STATIC_DRAW)
-
 
 position_loc = glGetAttribLocation(shader_program, "position")
 texcoord_loc = glGetAttribLocation(shader_program, "texcoord")
@@ -79,43 +71,23 @@ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
 clock = pygame.time.Clock()
 
-
-
 running = True
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
     glClear(GL_COLOR_BUFFER_BIT)
-
-
     glUseProgram(shader_program)
-
-
     glActiveTexture(GL_TEXTURE0)
     glBindTexture(GL_TEXTURE_2D, texture_id)
     glUniform1i(glGetUniformLocation(shader_program, "texture_sampler"), 0)
-
-
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
 
-
     fps = clock.get_fps()
-
     fps_str = str(round(fps))
-
     pygame.display.set_caption(f"FPS: {fps_str}")
-
-
-
-
     pygame.display.flip()
-
-
     clock.tick()
-
 
 pygame.quit()
