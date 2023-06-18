84772dd950c13204545e02a4f3ff9f7ca4192c08
matrass3 <freeservermatras@gmail.com>
2023-06-18 22:13:27 +0300

    Код

--- main.py
+++ main.py
@@ -8,29 +8,32 @@ pygame.init()
 
 width, height = 1200, 900
 screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
-texture = pygame.image.load("texture.jpg")  # Замените "texture.png" на путь к вашему изображению
-texture_data = pygame.image.tostring(texture, "RGBA", 1)
 
 vertex_shader = """
 #version 330
 in vec2 position;
-in vec2 texcoord;
 out vec2 frag_texcoord;
 void main()
 {
     gl_Position = vec4(position, 0.0, 1.0);
-    frag_texcoord = texcoord;
+    frag_texcoord = (position + vec2(1.0)) / 2.0;  // Преобразуем положение вершины в текстурные координаты
 }
 """
 
 fragment_shader = """
 #version 330
 in vec2 frag_texcoord;
-uniform sampler2D texture_sampler;
 out vec4 frag_color;
+uniform float time;
+
 void main()
 {
-    frag_color = texture(texture_sampler, frag_texcoord);
+    float speed = 1.0;  // Скорость изменения цвета
+    float r = (sin(time * speed) + 1.0) * 0.5;  // Изменение красного компонента
+    float g = (cos(time * speed) + 1.0) * 0.5;  // Изменение зеленого компонента
+    float b = (sin(time * speed * 0.7) + 1.0) * 0.5;  // Изменение синего компонента
+
+    frag_color = vec4(r, g, b, 1.0);  // Устанавливаем цвет фрагмента
 }
 """
 
@@ -40,10 +43,10 @@ shader_program = compileProgram(
 )
 
 vertex_data = np.array([
-    -1, -1, 0, 0,
-    -1, 1, 0, 1,
-    1, 1, 1, 1,
-    1, -1, 1, 0
+    -1, -1,
+    -1, 1,
+    1, 1,
+    1, -1
 ], dtype=np.float32)
 vertex_buffer = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
@@ -55,39 +58,36 @@ glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer)
 glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_data.nbytes, index_data, GL_STATIC_DRAW)
 
 position_loc = glGetAttribLocation(shader_program, "position")
-texcoord_loc = glGetAttribLocation(shader_program, "texcoord")
+
 glEnableVertexAttribArray(position_loc)
-glEnableVertexAttribArray(texcoord_loc)
-glVertexAttribPointer(position_loc, 2, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(0))
-glVertexAttribPointer(texcoord_loc, 2, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(8))
+glVertexAttribPointer(position_loc, 2, GL_FLOAT, GL_FALSE, 8, ctypes.c_void_p(0))
 
-texture_id = glGenTextures(1)
-glBindTexture(GL_TEXTURE_2D, texture_id)
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.get_width(), texture.get_height(), 0, GL_RGBA, GL_UNSIGNED_BYTE,
-             texture_data)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
+time_loc = glGetUniformLocation(shader_program, "time")
 
 clock = pygame.time.Clock()
+fps_update_interval = 1000  # Интервал обновления FPS в миллисекундах
+fps_timer = 0
 
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     glClear(GL_COLOR_BUFFER_BIT)
     glUseProgram(shader_program)
-    glActiveTexture(GL_TEXTURE0)
-    glBindTexture(GL_TEXTURE_2D, texture_id)
-    glUniform1i(glGetUniformLocation(shader_program, "texture_sampler"), 0)
+    glUniform1f(time_loc, pygame.time.get_ticks() / 1000.0)
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
 
-    fps = clock.get_fps()
-    fps_str = str(round(fps))
-    pygame.display.set_caption(f"FPS: {fps_str}")
     pygame.display.flip()
+
+    # Обновляем FPS раз в секунду
+    current_time = pygame.time.get_ticks()
+    if current_time - fps_timer >= fps_update_interval:
+        fps = clock.get_fps()
+        pygame.display.set_caption(f"OpenGL Example | FPS: {int(fps)}")
+        fps_timer = current_time
+
     clock.tick()
 
 pygame.quit()
