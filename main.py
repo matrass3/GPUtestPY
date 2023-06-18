9f16c8f99993592789305b8a9e64c6d12c3c2e4f
matrass3 <freeservermatras@gmail.com>
2023-06-18 15:59:25 +0300

    Теперь генерируються рандомные цвета

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 import random
+
 # Инициализация Pygame
 pygame.init()
 
 # Создание окна
-width, height = 800, 600
+width, height = 1200, 900
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("GPU Load")
 
@@ -16,11 +17,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Генерация случайного цвета
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+
     # Очистка экрана
     screen.fill((0, 0, 0))
 
-    # Отрисовка прямоугольника на экране
-    pygame.draw.rect(screen, (255, 255, 255), (0, 0, width, height))
+    # Отрисовка прямоугольника на экране с случайным цветом
+    pygame.draw.rect(screen, color, (0, 0, width, height))
 
     # Обновление экрана
     pygame.display.flip()
