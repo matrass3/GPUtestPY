4d75e1ba839c2058b2a1572450352eca825bae53
matrass3 <freeservermatras@gmail.com>
2023-06-18 16:11:03 +0300

    Добавление текстуры

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ import random
 pygame.init()
 
 # Создание окна
-width, height = 1200, 900
+width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("GPU Load")
 
-# Определение цветовой палитры
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]  # Пример палитры из трех цветов: красный, зеленый, синий
+# Загрузка текстуры
+texture = pygame.image.load("texture.jpg")  # Замените "texture.png" на путь к вашему изображению
+
+# Создание поверхности для рендеринга с более высоким разрешением
+render_width, render_height = 1600, 1200
+if texture.get_width() > render_width or texture.get_height() > render_height:
+    scale_factor = min(render_width / texture.get_width(), render_height / texture.get_height())
+    texture = pygame.transform.scale(texture, (int(texture.get_width() * scale_factor),
+                                               int(texture.get_height() * scale_factor)))
+    render_width, render_height = texture.get_width(), texture.get_height()
+
+render_surface = pygame.Surface((render_width, render_height))
 
 # Создание объекта для отслеживания времени
 clock = pygame.time.Clock()
@@ -23,15 +33,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Генерация случайного индекса цвета из палитры
-    color_index = random.randint(0, len(colors) - 1)
-    color = colors[color_index]
+    # Генерация случайных координат для отображения текстуры
+    x = random.randint(0, render_width - texture.get_width())
+    y = random.randint(0, render_height - texture.get_height())
 
-    # Очистка экрана
-    screen.fill((0, 0, 0))
+    # Очистка поверхности для рендеринга
+    render_surface.fill((0, 0, 0))
 
-    # Отрисовка прямоугольника на экране с случайным цветом
-    pygame.draw.rect(screen, color, (0, 0, width, height))
+    # Отрисовка текстуры на поверхности для рендеринга
+    render_surface.blit(texture, (x, y))
+
+    # Масштабирование поверхности для рендеринга до размеров окна
+    scaled_surface = pygame.transform.scale(render_surface, (width, height))
+
+    # Отображение поверхности для рендеринга на экране
+    screen.blit(scaled_surface, (0, 0))
 
     # Отображение FPS
     fps = int(clock.get_fps())
