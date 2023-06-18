2bfbe7e1ebbe530c1cf0dfd0e9008d54c3aaf401
matrass3 <freeservermatras@gmail.com>
2023-06-18 22:50:58 +0300

    Код

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
+from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileShader, compileProgram
 import numpy as np
-import math
+import random
 
 pygame.init()
 
 width, height = 1200, 900
 screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+pygame.display.set_caption("блять сука нахуй ебать")
 
 vertex_shader = """
 #version 330
@@ -16,7 +18,7 @@ out vec2 frag_texcoord;
 void main()
 {
     gl_Position = vec4(position, 0.0, 1.0);
-    frag_texcoord = (position + vec2(1.0)) / 2.0;  // Преобразуем положение вершины в текстурные координаты
+    frag_texcoord = (position + vec2(1.0)) / 2.0;
 }
 """
 
@@ -26,14 +28,33 @@ in vec2 frag_texcoord;
 out vec4 frag_color;
 uniform float time;
 
+float random(float seed) {
+    return fract(sin(seed) * 43758.5453);
+}
+
+vec3 randomColor(float seed) {
+    return vec3(random(seed), random(seed + 1.0), random(seed + 2.0));
+}
+
 void main()
 {
-    float speed = 1.0;  // Скорость изменения цвета
-    float r = (sin(time * speed) + 1.0) * 0.5;  // Изменение красного компонента
-    float g = (cos(time * speed) + 1.0) * 0.5;  // Изменение зеленого компонента
-    float b = (sin(time * speed * 0.7) + 1.0) * 0.5;  // Изменение синего компонента
+    vec2 center = vec2(0.5);
+    float radius = 0.5;
 
-    frag_color = vec4(r, g, b, 1.0);  // Устанавливаем цвет фрагмента
+    float distance = length(frag_texcoord - center);
+    float gradient = smoothstep(radius - 0.02, radius + 0.02, distance);
+
+    float transitionSpeed = 0.001; // Скорость перехода между цветами
+
+    float seed1 = floor(time * transitionSpeed);
+    float seed2 = seed1 + 1.0;
+
+    vec3 color1 = randomColor(seed1);
+    vec3 color2 = randomColor(seed2);
+
+    vec3 finalColor = mix(color1, color2, gradient);
+
+    frag_color = vec4(finalColor, 1.0);
 }
 """
 
@@ -58,36 +79,30 @@ glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer)
 glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_data.nbytes, index_data, GL_STATIC_DRAW)
 
 position_loc = glGetAttribLocation(shader_program, "position")
-
-glEnableVertexAttribArray(position_loc)
-glVertexAttribPointer(position_loc, 2, GL_FLOAT, GL_FALSE, 8, ctypes.c_void_p(0))
-
 time_loc = glGetUniformLocation(shader_program, "time")
 
-clock = pygame.time.Clock()
-fps_update_interval = 1000  # Интервал обновления FPS в миллисекундах
-fps_timer = 0
+glEnableVertexAttribArray(position_loc)
+glVertexAttribPointer(position_loc, 2, GL_FLOAT, GL_FALSE, 0, None)
 
-running = True
-while running:
+glClearColor(0.0, 0.0, 0.0, 1.0)
+glViewport(0, 0, width, height)
+
+clock = pygame.time.Clock()
+start_time = pygame.time.get_ticks()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            quit()
+
+    _time = (pygame.time.get_ticks() - start_time) / 1000.0
+    glUseProgram(shader_program)
+    glUniform1f(time_loc, _time)
 
     glClear(GL_COLOR_BUFFER_BIT)
-    glUseProgram(shader_program)
-    glUniform1f(time_loc, pygame.time.get_ticks() / 1000.0)
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, None)
 
+    pygame.display.set_caption(f"блять сука нахуй (FPS: {clock.get_fps():.2f})")
     pygame.display.flip()
-
-    # Обновляем FPS раз в секунду
-    current_time = pygame.time.get_ticks()
-    if current_time - fps_timer >= fps_update_interval:
-        fps = clock.get_fps()
-        pygame.display.set_caption(f"OpenGL Example | FPS: {int(fps)}")
-        fps_timer = current_time
-
-    clock.tick()
-
-pygame.quit()
+    clock.tick(1000000)
